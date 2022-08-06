aa240a3d01ac894eef1f6b8758e5e2e2caf9bd0b
Nicolas Espana ribera <niespana@fu-r3-p3.s19.be>
2022-08-06 18:38:18 +0200

    yey

--- test.c
+++ test.c
@@ -42,10 +42,9 @@ void	select_op(int op, t_list **a, t_list **b)
 void display1(int *lst)
 {
 	int i = -1;
-	ft_printf("------------\n");
 	while (lst[++i] > -1)
 		ft_printf("[\e[0;31m%d\e[0m]", lst[i]);
-	ft_printf("-------------\n");
+	write(0, "\n",1);
 }
 int lcontain(int *lst, int pos)
 {
@@ -59,17 +58,83 @@ int lcontain(int *lst, int pos)
 	}
 	return (0);
 }
-
-int sort(t_list *a, t_list *b, int *elim)
+static void change_list(int **nbs, int posi)
 {
-	if (b && b->pos == a->pos - 1)
+	int pos;
+	
+	pos = 0;
+	display1(*nbs);
+	while ((*nbs)[pos] >= 0)
+		pos++;	
+	(*nbs)[pos++] = posi;
+	(*nbs)[pos] = -1;
+	display1(*nbs);
+}
+void display_pos(t_list *a)
+{
+	while (a)
+	{
+		ft_printf("[%d:%d]",a->pos, a->nb);
+		a = a->next;
+	}
+	ft_printf("\n");
+}
+int is_kinda_sorted(t_list *a)
+{
+	t_list	*start = a;
+	int		smol;
+
+	smol = -1;
+	while (a && a->pos != 0)
+		a = a->next;
+	while (a)
+	{
+		if (a->pos != ++smol)
+			return (0);
+		a = a->next;
+	}
+	while (start && start->pos != 0)
+	{
+		if (start->pos != ++smol)
+			return (0);
+		start = start->next;
+	}
+	return (1);
+}
+int direction_to_pos(t_list *a, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (a)
+	{
+		i++;
+		a = a->next;
+	}
+	return (i < size(a) / 2);
+}
+/*************************************************/
+int sort(t_list *a, t_list *b, int **elim)
+{
+	if (!b && is_kinda_sorted(a))
+	{
+		if (direction_to_pos(a, 0))
+			return (9);
+		else
+			return (6);
+	}
+	else if (a->pos == a->next->next->pos - 1 || get_last(a)->pos == a->next->pos - 1 || a->pos - 1 == a->next->pos)
+		return (1);
+	else if (b && (b->pos == a->pos - 1 || b->pos == get_last(a)->pos + 1))
+	{
+		change_list(elim, b->pos);
 		return (4);
-	else if (!lcontain(elim, a->pos)
-		&& (a->pos == a->next->pos -  1 || a->pos == get_last(a)->pos + 1))
+	}
+	else if (!lcontain(*elim, a->pos))
 		return (5);
 	return (6);
 }
-
+/*************************************************/
 int main(int ac, char **av)
 {
 	t_list	*a;
@@ -83,6 +148,7 @@ int main(int ac, char **av)
 	if (ac == 1)
 		return (0);
 	a = get_stack(av, 0, 0);
+
 	assign_stack(&a, get_smallest(a));
 	display_list(a);
 	int *pushb = to_push(a, 0,0, size(a));
@@ -92,12 +158,13 @@ int main(int ac, char **av)
 		ft_printf("[%d]", pushb[i]);
 	ft_printf("\n");
 	int l = 0;
+	display_pos(a);
 	while (!is_sorted(a, b) && ++l)
 	{
-		ft_printf("-------%d-------\n", l + 1);
-		if (l > 30)
+		ft_printf("-------%d-------\n", l);
+		if (l > 50)
 			exit(0);
-		select_op(sort(a,b, pushb), &a, &b);
+		select_op(sort(a,b, &pushb), &a, &b);
 		ft_printf("a:\n");
 		display_list(a);
 		ft_printf("b:\n");
