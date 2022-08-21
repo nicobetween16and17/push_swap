81a3e3f4a82f5527da07fb7f48b93e6c1f404213
Nicolas Espana ribera <niespana@fu-r3-p3.s19.be>
2022-08-21 08:02:28 +0200

    Jk

--- ope3.c
+++ ope3.c
@@ -12,26 +12,6 @@
 
 #include "push_swap.h"
 
-int	get_nearest_smallest(t_list *a)
-{
-	t_list	*end;
-	int		i;
-	int		smallest;
-
-	i = -1;
-	smallest = a->nb;
-	end = get_last(a);
-	while (++i < 1)
-	{
-		if (a->next && smallest > a->next->nb)
-			smallest = a->next->nb;
-		if (smallest > end->nb)
-			smallest = end->nb;
-		a = a->next;
-	}
-	return (smallest);
-}
-
 int	contain(t_list *a, int n)
 {
 	while (a && a->next)
@@ -59,13 +39,7 @@ int	is_inverse_sorted(t_list *b)
 	}
 	return (1);
 }
-/*-----*
-* Le premier while stop directement si la cible est au bout
-* sinon incremente jusqu'a trouver le max puis, compare un par un les nombre adjacents
-* ensuite on repart du debut en comparant depuis le dernier et ce, jusqu'au maximum
-* si la suite de comparaison prouve que nb1 > nb2 > nb3 > ... > nbfin alors retourne vrai
-* si une comparaison est fausse, renvoit faux
-*------*/
+
 int is_soft_invert_sorted(t_list *b, int rdy)
 {
 	int		bik;
--- test.c
+++ test.c
@@ -16,12 +16,12 @@ void	select_op2(t_list **a, t_list **b, int op)
 		reverse_rotate(a);
 		reverse_rotate(b);
 	}
-	ft_printf("\e[0m");
 }
 
 void	select_op(int op, t_list **a, t_list **b)
 {
-	ft_printf("\e[0;35m");
+	if (!op)
+		return ;
 	if (op == 1 && ft_printf("sa\n"))
 		swap(a);
 	if (op == 2 && ft_printf("sb\n"))
@@ -41,76 +41,50 @@ void	select_op(int op, t_list **a, t_list **b)
 		rotate(b);
 	select_op2(a, b, op);
 }
-void display_pos(t_list *a)
-{
-	while (a)
-	{
-		ft_printf("[%d:%d]",a->pos, a->nb);
-		a = a->next;
-	}
-	ft_printf("\n");
-}
-int is_kinda_sorted(t_list *a)
-{
-	t_list	*start = a;
-	int		smol;
 
-	smol = -1;
-	while (a && a->pos != 0)
-		a = a->next;
-	while (a)
-	{
-		if (a->pos != ++smol)
-			return (0);
-		a = a->next;
-	}
-	while (start && start->pos != 0)
-	{
-		if (start->pos != ++smol)
-			return (0);
-		start = start->next;
-	}
-	return (1);
-}
-
-int soft_sorted(t_list *a)
-{
-	int small;
-	int current;
-	t_list *start;
-
-	start = a;
-	current = get_smallest_pos(a);
-	small = current;
-	while (a && a->pos != small)
-		a = a->next;
-	a = a->next;
-	while (a)
-	{
-		if (a->pos != ++current)
-			return (0);
-		a = a->next;
-	}
-	a = start;
-	while (a && a->pos != small)
-	{
-		if (a->pos != ++current)
-			return (0);
-		a = a->next;
-	}
-	return (1);
-}
 
 int radix(int pos, int bit)
 {
-	int res;
-
 	while (bit)
 	{
 		bit--;
-		res /= 2;
+		pos /= 2;
 	}
-	return (res % 2);
+	return (pos % 2);
+}
+
+int radix_fin(t_list *a, int bit)
+{
+	while (a)
+	{
+		if (!radix(a->pos, bit))
+			return (0);
+		a = a->next;
+	}
+	return (1);
+}
+int get_dir(t_list *a, int pos, int size, int reset)
+{
+	static int res;
+	int i;
+
+	if (reset)
+		res = 0;
+	if (res)
+		return (res);
+	i = 0;
+	while (a)
+	{
+
+		if (a->pos == pos)
+			break;
+		i++;
+		a = a->next;
+	}
+	res = 6;
+	if (i <= size / 2)
+		res = 9;
+	return (res);
 }
 /*************************************************/
 int sort(t_list *a, t_list *b)
@@ -118,35 +92,40 @@ int sort(t_list *a, t_list *b)
 	static int bit;
 	static int step;
 	static int first;
-	static int position;
 
-	if (is_kinda_sorted(a) && !b)
-		return (6);
-	if (step)
+	if (step == 1)
 	{
-		ft_printf("\e[0;31m-----------------------------------\e[0m\n");
+		if (a->pos == first)
+			step++;
+		else
+			return (get_dir(a, first, size(a), 0));
+	}
+	if (step == 2)
+	{
+		get_dir(a, first, size(a), 1);
 		if (b)
 			return (4);
 		else
 		{
-			step--;
+			first = 0;
+			step = 0;
 			bit++;
-			position = 0;
 		}
 	}
 	if (!step)
 	{
-		if (first && a->pos == first)
+		if (radix_fin(a, bit))
 			step++;
-		if (!first && (!radix(a->pos, bit) || a->pos == position))
-			first = a->pos;
-		printf("%d != %d\n", a->pos, position);
-		if (!radix(a->pos, bit) || a->pos == position++)
-			return (6);
+		else if (radix(a->pos, bit))
+		{
+			if (!first)
+				first = a->pos;
+			return (9);
+		}
 		else
 			return (5);
 	}
-	return (1);
+	return (0);
 }
 /*************************************************/
 int main(int ac, char **av)
@@ -154,32 +133,15 @@ int main(int ac, char **av)
 	t_list	*a;
 	t_list	*b;
 
-	static int	nb;
-	if (!nb)
-		nb = 1;
 	b = NULL;
 	if (ac == 1)
 		return (0);
-	a = get_stack(av, 0, 0);int i = -1;
-	while (++i < 11)
-		ft_printf("%d", radix(521, i));
-	exit(0);
+	a = get_stack(av, 0, 0);
 	assign_stack(&a, get_smallest(a));
-	display_list(a);
-
-	ft_printf("\n");
-	int l = 0;
-	display_pos(a);
 	while (!is_sorted(a, b))
-	{
-		ft_printf("-------%d-------\n", l);
-		if (l++ > 150)
-			exit(0);
 		select_op(sort(a, b), &a, &b);
-		ft_printf("a:\n");
-		display_list(a);
-		ft_printf("b:\n");
-		display_list(b);
-	}
-
+	/*ft_printf("a:\n");
+	display_list(a);
+	ft_printf("b:\n");
+	display_list(b);*/
 }
